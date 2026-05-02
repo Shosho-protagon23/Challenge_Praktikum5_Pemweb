3ae48bc7446990f98fd43380a531e3f9325b8aa3
Faga Imam Wicaksono <faga.defgh@gmail.com>
2026-05-02 18:32:24 +0700

    add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var namaLengkap = document.getElementById("namaLengkap");
+var email = document.getElementById("email");
+var jK = document.getElementById("jk");
+var kota = document.getElementById("kota");
